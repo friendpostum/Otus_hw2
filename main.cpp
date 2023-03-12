12147e6eb0a05db89f2bfbcd1177d292787902f6
HP <gamper74@gmail.com>
2023-03-12 10:31:41 +0500

    work on mistakes final

--- main.cpp
+++ main.cpp
@@ -5,25 +5,24 @@ int main() {
     try {
       pool_ip ip_pool;
 
-        for(std::string line; std::getline(std::cin, line);) {
-            auto v = split(line);
-            ip_pool.push_back(v);
-        }
+      for(std::string line; std::getline(std::cin, line);) {
+          auto v = split(line);
+          ip_pool.push_back(v);
+      }
 
-        radixSort(ip_pool);
+      radixSort(ip_pool);
 
-        print_pool(ip_pool.begin(), ip_pool.end());
+      print_pool(ip_pool.begin(), ip_pool.end());
 
-        filter(ip_pool, 1);
+      filter(ip_pool, 1);
 
-        filter(ip_pool, 46, 70);
+      filter(ip_pool, 46, 70);
 
-        filter_any(ip_pool, 46);
+      filter_any(ip_pool, 46);
 
+    } catch (const std::exception &e) {
+      std::cerr << e.what() << std::endl;
     }
-    catch(const std::exception &e) {
-        std::cerr << e.what() << std::endl;
-    }
 
-    return 0;
+  return 0;
 }
